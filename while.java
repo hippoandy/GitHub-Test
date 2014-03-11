5c2eb5100bc9b1c2bdf2253ef70c3441564d707c
何昱璋 <loveangela1993928@gmail.com>
2014-03-11 11:43:54 +0800

    While loop test

---
+++ while.java
@@ -0,0 +1,16 @@
+public class while
+{
+	while M = new while();
+	
+	public static void main( String[] args )
+	{
+		M.Start();
+	}
+
+	void Start()
+	{
+		System.out.print( "Hello World!!" );
+	}
+
+
+}
